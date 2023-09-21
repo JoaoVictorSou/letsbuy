a8424d546b4c7833c096e113f6d3dedbf4b3bf94
JoaoVictorSou <soulima.joao@gmail.com>
2023-09-21 19:02:55 -0300

    feat: new products image

--- src/screen/MarketRain/index.jsx
+++ src/screen/MarketRain/index.jsx
@@ -2,8 +2,29 @@ import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Image, Dimensions, Alert } from "react-native";
 
 import ruffles_image from '../../../static/products/ruffles.png'
-import glade_image from '../../../static/products/glade-johnson.png'
 import camil_cookie_image from '../../../static/products/rosquinha-camil.png'
+import andorinha_oil from '../../../static/products/azeite-andorinha.png'
+import bayer_bepantol from '../../../static/products/bepantol-bayer.png'
+import grendene_slipper from '../../../static/products/chinelo-grendene.png'
+import linea_chocolate from '../../../static/products/chocolate-linea.png'
+import lanossi_elastic from '../../../static/products/elastico-lanossi.png'
+import porridge from '../../../static/products/farinha-lactea.png'
+import modess_evergreen from '../../../static/products/modess-evergreen.png'
+//import dacolonia_sweet_peanut from '../../../static/products/pe-moleque-dacolonia.png'
+import rayovac_battery from '../../../static/products/pilha-rayovac.png'
+import aurora_tabasco_pepper from '../../../static/products/pimenta-tabasco-aurora.png'
+import urca_solid_soap from '../../../static/products/sabao-pedra-urca.png'
+import nivea_milk_soap from '../../../static/products/sabonete-milk-nivea.png'
+import nivea_soap from '../../../static/products/sabonete-nivea.png'
+import mars_skittles from '../../../static/products/skittles-mars.png'
+import diageo_smirnoff from '../../../static/products/smirnoff-diageo.png'
+import mars_snickers from '../../../static/products/snickers-mars.png'
+import fante_juice from '../../../static/products/suco-fante.png'
+import hair_dye_duty from '../../../static/products/tinta-duty.png'
+import pepsico_toddynho from '../../../static/products/toddynho-pepsico.png'
+import mars_twix from '../../../static/products/twix-mars.png'
+import valduga_arte_wine from '../../../static/products/vinho-arte-valduga.png'
+import marspet_wiskas from '../../../static/products/wiskas-marspet.png'
 import urca_soap_image from '../../../static/products/sabao-urca.png'
 import basket_image from '../../../static/structure/basket.png'
 
@@ -14,8 +35,28 @@ const image_square = width
 const basket_square = 250
 const products = [
     ruffles_image,
-    glade_image,
     camil_cookie_image,
+    andorinha_oil,
+    bayer_bepantol,
+    grendene_slipper,
+    linea_chocolate,
+    lanossi_elastic,
+    porridge,
+    modess_evergreen,
+    rayovac_battery,
+    aurora_tabasco_pepper,
+    urca_solid_soap,
+    nivea_milk_soap,
+    nivea_soap,
+    mars_skittles,
+    diageo_smirnoff,
+    mars_snickers,
+    fante_juice,
+    hair_dye_duty,
+    pepsico_toddynho,
+    mars_twix,
+    valduga_arte_wine,
+    marspet_wiskas,
     urca_soap_image
 ]
 
