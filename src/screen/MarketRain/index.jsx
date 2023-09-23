5a115afca3a425bc64b8b14dde7bc6be92b8a7ed
JoaoVictorSou <soulima.joao@gmail.com>
2023-09-23 10:42:40 -0300

    feat: height capture area

--- src/screen/MarketRain/index.jsx
+++ src/screen/MarketRain/index.jsx
@@ -11,30 +11,37 @@ import { get_image_dimensions } from "../../util/data_treatment";
 
 const screen_width = Dimensions.get('window').width
 const screen_height = Dimensions.get('window').height
-const basket_square = 250
-const fall_pixels = screen_height/8
-const left_move_max =  Math.round(screen_width/8)
+const basket_width = 251
+const basket_height = 200
+const fall_pixels = screen_height/15
+const left_move_max =  Math.round(screen_width/9)
 const left_move_min = left_move_max*-1
 
 function product_move (coordinates, image_dimensions, win, screen_width, changeCoordinates, changeWinStatus) {
-    let top_increment = Math.floor(Math.random() * (fall_pixels + 1))
+    let top_increment = Math.random() * (fall_pixels + 1)
     let new_top = coordinates.top + top_increment
     
     let new_left = coordinates.left
-    const move = Math.floor(Math.random() * (2 + 1))
+    let left_increment = 0
+    const move = Math.floor(Math.random() * (3 + 1))
     
     if (move == 1) {
         if (coordinates.left <= 0) {
-            let increment = Math.floor(Math.random() * (left_move_max + 1))
-            new_left = new_left+increment
+            left_increment = Math.random() * (left_move_max + 1)
         } else if (coordinates.left >= screen_width-image_dimensions.width) {
-            let increment = Math.floor(Math.random() * (left_move_max + 1))
-            new_left = new_left-increment
+            left_increment = Math.random() * (left_move_max + 1) * -1
         } else {
-            let increment = Math.floor(Math.random() * (left_move_max - left_move_min + 1)) + left_move_min
-            new_left = new_left+increment
+            left_increment = Math.random() * (left_move_max - left_move_min + 1) + left_move_min
         }
     }
+
+    if(coordinates.left >= screen_width*1.9 && left_increment > 0) {
+        left_increment = 0
+    } else if (coordinates.left <= image_dimensions && left_increment < 0) {
+        left_increment = 0
+    } 
+    
+    new_left += left_increment
 
     changeCoordinates({
         left: new_left, 
@@ -54,12 +61,12 @@ function random_left_coordinates(image_dimensions) {
     return left_coordinate
 }
 
-function win_verify(win, coordinates, image_dimensions, basket_square, screen_width, screen_height, changeWinStatus) {
-    const basket_position_y = screen_height-basket_square
-    const basket_position_x = (screen_width/2)-(basket_square/2)
+function win_verify(win, coordinates, image_dimensions, basket_width, screen_width, screen_height, changeWinStatus) {
+    const basket_position_y = screen_height-basket_width
+    const basket_position_x = (screen_width/2)-(basket_width/2)
 
-    let top_aligned = coordinates.top >= basket_position_y && coordinates.top <= screen_height
-    let left_aligned = coordinates.left >= basket_position_x && coordinates.left <= (basket_position_x+basket_square-image_dimensions.width)
+    let top_aligned = coordinates.top >= basket_position_y && coordinates.top <= (basket_position_y+basket_height)-image_dimensions.height
+    let left_aligned = coordinates.left >= basket_position_x && coordinates.left <= (basket_position_x+basket_width-image_dimensions.width)
     
     if (top_aligned && left_aligned) {
         changeWinStatus(true)
@@ -87,9 +94,9 @@ const MarketRain = (_) => {
             if (gameState != 0) {
                 product_move(coordinates, image_dimensions, win, screen_width, changeCoordinates, changeWinStatus)
             }
-        }, 100)
+        }, 50)
 
-        win_verify(win, coordinates, image_dimensions, basket_square, screen_width, screen_height, changeWinStatus)
+        win_verify(win, coordinates, image_dimensions, basket_width, screen_width, screen_height, changeWinStatus)
         
         if ((coordinates.top > screen_height) || win) {
             if (gameState == 1) {
@@ -146,7 +153,6 @@ const MarketRain = (_) => {
 const styles = StyleSheet.create({
     market_rain_wrapper: {
         position: "relative",
-        //backgroundColor: "#f1f1f1", //"#223D73",
         width: screen_width,
         height: screen_height
     },
@@ -160,10 +166,10 @@ const styles = StyleSheet.create({
         position: "absolute"
     },
     basket_area: {
-        top: screen_height-basket_square,
-        left: (screen_width/2)-(basket_square/2),
-        width: basket_square,
-        height: basket_square,
+        top: screen_height-basket_width,
+        left: (screen_width/2)-(basket_width/2),
+        width: basket_width,
+        height: basket_height,
         alignItems: "center"
     },
     corporation_logo: {
